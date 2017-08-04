03dc2e8428fc525b31bd16fa82aa9f50603e51be
CUCHOT Florian <fcuchot@axones.com>
2017-08-04 11:42:05 +0200

    Created Solution

---
+++ PortFolioSolution/PortFolioWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PortFolioWeb.Startup))]
+namespace PortFolioWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
